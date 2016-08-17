b951c0a0c492cc56ec13edc23b55d3b322a0ca38
brianou <409288393@qq.com>
2016-08-17 11:55:20 +0800

    布局

--- app/component/Account.js
+++ app/component/Account.js
@@ -42,9 +42,20 @@ class Account extends Component {
         console.log('render');
         return (<div>
             <h1>This is account</h1>
-            <div>
+            <div ref="accountInfo" style={{width:'80%',margin:'auto'}}>
+                <div className="col-xs-3" style={{textAlign:'right'}}>name:</div>
+                <input ref="userName" className="col-xs-3" type="text"
+                       onChange={this.onChangeFieldHandler.bind(this)} placeholder="please input user name"
+                       value={this.state.account.userName}/>
 
-
+                <div className="col-xs-3" style={{textAlign:'right'}}>age:</div>
+                <input ref="age" type="text" className="col-xs-3" onChange={this.onChangeFieldHandler.bind(this)}
+                       placeholder="please input user age" value={this.state.account.age}/>
+            </div>
+            <div className="col-xs-12">
+                <input type="button" className="btn-default" value="Submit"
+                       onClick={this.onCreateAccount.bind(this)}/>
+                <label>{this.props.account.userName}</label>
             </div>
         </div>);
     }
